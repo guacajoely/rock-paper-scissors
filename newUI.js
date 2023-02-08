f038b2a208ff53686ceecfe4a54c98660fa5b1dc
guacajoely <guacajoely@gmail.com>
2023-02-08 10:45:47 -0500

    Added UI that takes user input via 3 buttons

---
+++ newUI.js
@@ -0,0 +1,107 @@
+//alternate version of MyWay.js using 3 buttons for each input instead of text
+
+//GET COMPUTERS CHOICE
+function getComputerChoice() {
+
+    randomSelection = Math.floor((Math.random() * 3) +1);
+    
+        if (randomSelection === 1){
+            return 'ROCK';
+        }
+    
+        else if (randomSelection === 2){
+            return 'PAPER';
+        }
+    
+        else {
+            return 'SCISSORS'
+        }
+    
+     }
+    
+    
+    //DECLARATIONS
+    let button = document.querySelector("button")
+    let result = document.getElementById('result')
+    let playerText = document.getElementById('player-select')
+    let compText = document.getElementById('comp-select')
+    let playerSelection = " "
+    let computerSelection = " "
+    let wins = 0
+    let losses = 0
+    let winOrLoss
+    let score = document.getElementById('score')
+    let finalResult = document.getElementById('final-result')
+    
+    
+    //GET PLAYER'S CHOICE AND OUTPUT RESULT WHEN A BUTTON CORRESPONDING TO THE SELECTION IS CLICKED
+
+    const buttons = document.querySelectorAll('.btn'); //and the key variable is wrapped in "array.from()
+    buttons.forEach(id => id.addEventListener("click",  function(e){
+
+        console.log(e.target.id)
+    
+        playerSelection = e.target.id
+        playerText.innerHTML = "You've selected: " + playerSelection + "</br>";
+    
+        computerSelection = getComputerChoice()
+        compText.innerHTML = 'The computer selected: ' + computerSelection + "</br>";
+    
+            if(playerSelection === computerSelection){
+                console.log('TIE')
+                result.innerHTML = `TIE! You and the computer both chose <strong>${computerSelection}</strong>. </br> Please try again.`
+                return
+            }
+    
+            else if ((playerSelection === 'ROCK' && computerSelection === 'SCISSORS') || 
+                    (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
+                    (playerSelection === 'PAPER' && computerSelection === 'ROCK') ) {
+                        console.log('YOU WIN')
+                        result.innerHTML = `You WIN! <strong>${playerSelection}</strong> beats <strong>${computerSelection}</strong>`
+                        updateScore('win')
+                        return
+            }
+    
+            else {
+                console.log('YOU LOSE')
+    
+                //IF ANYTHING OTHER THAN ROCK, PAPER, OR SCISSORS IS ENTERED, THIS ALSO RESULTS IN A LOSS
+                result.innerHTML = `Sorry, you lose. <strong>${computerSelection}</strong> beats <strong>${playerSelection}</strong>`;
+                updateScore('loss')
+                return
+            }
+    
+      }))
+
+function updateScore(roundResult){
+
+    console.log(finalResult.innerHTML)
+
+    if(wins>2 || losses>2){
+        wins=0;
+        losses=0;
+        finalResult.innerHTML = '';
+    }
+
+    if (roundResult === 'win'){
+        wins += 1
+    }
+
+    else if (roundResult === 'loss'){
+        losses += 1
+    }
+
+console.log(roundResult)
+score.innerHTML = 'The current score is </br> <strong>You:' + wins + ' </br> Computer:' + losses + '</strong>'
+
+
+if (wins > 2) {
+finalResult.innerHTML = 'You WON the best of 5!'
+}
+
+else if(losses > 2) {
+finalResult.innerHTML = 'Sorry, you lost the best of 5.'
+}
+
+}
+
